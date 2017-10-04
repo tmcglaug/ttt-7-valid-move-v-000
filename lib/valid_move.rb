11e0d823f6a1ea9e719734c050a26c3fd954adcb
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-04 13:03:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,18 +5,9 @@ if index.to_i > 9
 else
   if index.to_i < 1
     false
-  else
-    if position_taken(board, index) == true
-      false
-
-
   end
 end
 
-end
-
-end
-end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
